3ca4ebc23c927b33cc0f03831735f7db790e344d
Alastair Cota <a1.cota.365@gmail.com>
2021-07-03 12:13:03 +0100

    Create LAMPws.py

---
+++ experimental/WebSockets/LAMPws.py
@@ -0,0 +1,13 @@
+#pip install websockets
+import asyncio, pathlib, ssl, websockets
+
+async def LAMP(websocket, path):
+    print("hello")
+    data = await websocket.recv()
+    print(f"< {data}")
+    await websocket.send(f"{data})")
+
+server = websockets.serve(LAMP, "0.0.0.0", 8765)
+asyncio.get_event_loop().run_until_complete(server)
+print("Server Running at ws://0.0.0.0:8765")
+asyncio.get_event_loop().run_forever()
