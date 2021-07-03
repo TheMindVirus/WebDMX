15ffe22be7c82e1e7750ec963a0bbf0cbad56245
Alastair Cota <a1.cota.365@gmail.com>
2021-07-03 12:37:21 +0100

    Update LAMPws.py

--- experimental/WebSockets/LAMPws.py
+++ experimental/WebSockets/LAMPws.py
@@ -2,10 +2,11 @@
 import asyncio, pathlib, ssl, websockets
 
 async def LAMP(websocket, path):
-    print("hello")
+    print(websocket)
     data = await websocket.recv()
     print(f"< {data}")
     await websocket.send(f"{data})")
+    websocket.close()
 
 server = websockets.serve(LAMP, "0.0.0.0", 8765)
 asyncio.get_event_loop().run_until_complete(server)
