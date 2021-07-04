ecca96e4888209bf34a1427ca9eefcec06e16881
Alastair Cota <a1.cota.365@gmail.com>
2021-07-04 16:53:04 +0100

    Updated Project Files to 2021
    A lot has been damaged.

--- project/Assets/CameraControl.cs
+++ project/Assets/CameraControl.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraControl : MonoBehaviour
 {
     float speed = 0.1f;
-    float sensitivity = 1.5f;
+    float sensitivity = 2.0f;
     float floor = 0.5f;
+    float deadzone = 0.1f;
     Vector3 facing = new Vector3(0, 0, 0);
     Vector3 direction = new Vector3(0, 0, 0);
     Vector3 newpos = new Vector3(0, 0, 0);
@@ -18,14 +20,26 @@ public class CameraControl : MonoBehaviour
 
     void Update()
     {
-        facing = new Vector3(transform.eulerAngles.x + (Input.GetAxis("Mouse Y") * sensitivity * -1.0f),
-                                transform.eulerAngles.y + (Input.GetAxis("Mouse X") * sensitivity), 0);
+        var gamepad = Gamepad.current;
+        if (gamepad == null) { return; }
+        Vector2 leftStick = gamepad.leftStick.ReadValue();
+        Vector2 rightStick = gamepad.rightStick.ReadValue();
+        leftStick = new Vector2((Mathf.Abs(leftStick.x) > deadzone) ? leftStick.x * sensitivity : 0.0f, (Mathf.Abs(leftStick.y) > deadzone) ? leftStick.y * sensitivity : 0.0f);
+        rightStick = new Vector2((Mathf.Abs(rightStick.x) > deadzone) ? rightStick.x * sensitivity : 0.0f, (Mathf.Abs(rightStick.y) > deadzone) ? rightStick.y * sensitivity : 0.0f);
+
+        //facing = new Vector3(transform.eulerAngles.x + (Input.GetAxis("Mouse Y") * sensitivity * -1.0f),
+        //                transform.eulerAngles.y + (Input.GetAxis("Mouse X") * sensitivity), 0);
+        //facing = new Vector3(transform.eulerAngles.x + (Input.GetAxis("LeftStickVertical") * sensitivity * -1.0f),
+        //                        transform.eulerAngles.y + (Input.GetAxis("LeftStickHorizontal") * sensitivity), 0);
+        facing = new Vector3(transform.eulerAngles.x + (rightStick.y * -1.0f),
+                                transform.eulerAngles.y + (rightStick.x), 0);
         transform.eulerAngles = facing;
 
-        if (Input.GetKey(KeyCode.W)) { direction += new Vector3( 0,  0,  1); }
-        if (Input.GetKey(KeyCode.S)) { direction += new Vector3( 0,  0, -1); }
-        if (Input.GetKey(KeyCode.A)) { direction += new Vector3(-1,  0,  0); }
-        if (Input.GetKey(KeyCode.D)) { direction += new Vector3( 1,  0,  0); }
+        //if (Input.GetKey(KeyCode.W)) { direction += new Vector3( 0,  0,  1); }
+        //if (Input.GetKey(KeyCode.S)) { direction += new Vector3( 0,  0, -1); }
+        //if (Input.GetKey(KeyCode.A)) { direction += new Vector3(-1,  0,  0); }
+        //if (Input.GetKey(KeyCode.D)) { direction += new Vector3( 1,  0,  0); }
+        direction = new Vector3(leftStick.x, 0, leftStick.y * -1.0f);
         direction = direction * speed;
         transform.Translate(direction);
         newpos = transform.position;
